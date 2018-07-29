1cb9d4af2e5e471dfe1c31c2e88318447c6b0ed7
Nick Cirone <cironen@yahoo.com>
2018-07-29 14:15:34 -0400

    added policy document model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,46 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Policy(models.Model):
+
+    POSSIBLE_TAGS = (
+        ('diversity', 'Diversity'),
+        ('admissions', 'Admissions'),
+        ('enrollment', 'Enrollment'),
+        ('faculty', 'Faculty'),
+        ('student affairs', 'Student Affairs'),
+        ('administrative', 'Administrative'),
+        ('fees', 'Fees'),
+        ('data measures', 'Data Measures'),
+        ('international students', 'International Students'),
+        ('academic standards', 'Academic Standards'),
+        ('athletics', 'Athletics'),
+        ('title ix', 'Title IX'),
+        ('accreditation','Accreditation')
+    )
+
+    title = models.CharField(max_length=300)
+    school = models.CharField(max_length=300)
+    department = models.CharField(max_length=300)
+    administrator = models.CharField(max_length=300)
+    author = models.CharField(max_length=300, blank=True)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    link = models.URLField()
+    published_date = models.DateField()
+    tags = models.CharField(choices=POSSIBLE_TAGS)
+    abstract = models.TextField()
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
