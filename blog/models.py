f1f1d5d6238eb832b171dae67f3e1ec54ff5a3f0
Nick Cirone <cironen@yahoo.com>
2018-08-02 21:11:26 -0400

    readded env

--- blog/models.py
+++ blog/models.py
@@ -34,28 +34,25 @@ class Policy(models.Model):
         ('academic standards', 'Academic Standards'),
         ('athletics', 'Athletics'),
         ('title ix', 'Title IX'),
-        ('accreditation','Accreditation')
+        ('accreditation', 'Accreditation')
     )
 
     title = models.CharField(max_length=300)
     school = models.CharField(max_length=300)
-    department = models.CharField(max_length=300)
-    administrator = models.CharField(max_length=300)
+    department = models.CharField(max_length=300, blank=True)
+    administrator = models.CharField(max_length=300, blank=True)
     author = models.CharField(max_length=300, blank=True)
     state = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     latitude = models.FloatField()
     longitude = models.FloatField()
     link = models.URLField()
-    published_date = models.DateField()
+    published_date = models.DateField(blank=True)
     tags = models.CharField(choices=POSSIBLE_TAGS)
-    abstract = models.TextField()
+    abstract = models.TextField(blank=True)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
 
     def publish(self):
-        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
