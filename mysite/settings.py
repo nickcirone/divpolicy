8d32569b130e124d0bd97bac90902be69524cb1c
Noah Omdal <nomdal@Noahs-MacBook-Pro-36.local>
2018-09-30 13:48:30 -0500

    adding newest code 9/29

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,7 +131,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'xsboediz',
         'USER': 'xsboediz',
-        'PASSWORD': 'Qcn3tmpnSgp1QHgk_dsCvukFFPJAofm0',
+        'PASSWORD': '',
         'HOST': 'baasu.db.elephantsql.com',
         'PORT': '5432'
     }
