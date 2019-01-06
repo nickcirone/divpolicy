03d95883528420dc07e30e2794eeba2c6e7fc121
Noah Omdal <nomdal@Noahs-MacBook-Pro-36.local>
2019-01-06 17:56:25 -0600

    Final updates before transferring git

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,22 @@ import json
 import ast
 
 # Create your views here.
+from django.core.paginator import Paginator, Page
+
+class DSEPaginator(Paginator):
+    """
+    Override Django's built-in Paginator class to take in a count/total number of items;
+    Elasticsearch provides the total as a part of the query results, so we can minimize hits.
+    """
+    def __init__(self, *args, **kwargs):
+        super(DSEPaginator, self).__init__(*args, **kwargs)
+        self._count = self.object_list.hits.total
+
+    def page(self, number):
+        # this is overridden to prevent any slicing of the object_list - Elasticsearch has
+        # returned the sliced data already.
+        number = self.validate_number(number)
+        return Page(self.object_list, number, self)
 
 def search_home(request):
     return render(request, 'blog/search_home.html')
@@ -16,6 +32,20 @@ def search_home(request):
 def policy_search(request):
     term = request.GET.get('search')
     policies = search(term)
+
+    page = int(request.GET.get('page', '1'))
+    start = (page - 1) * 10
+    end = start + 10
+
+
+    paginator = DSEPaginator(policies, 10)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/policy_list.html', {'policies': policies})
 
 
